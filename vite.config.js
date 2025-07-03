16cd60701bb75d0dbf13e995e6a5f2dfe6293279
Aadil <aadilkhan3007@gmail.com>
2025-07-04 01:04:16 +0530

    Axios Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    build: {
+    /*build: {
         rollupOptions: {
             output: {
                 manualChunks(id) {
@@ -17,5 +17,5 @@ export default defineConfig({
                 },
             },
         },
-    },
+    },*/
 });
